3b96dbac91e73f4eb3e7710f04bb574a1b4be71c
Jamina Pöllänen <bgj428@myy.haaga-helia.fi>
2020-11-17 16:01:11 +0200

    korjailua

--- src/test/java/com/example/loytokoti/AnimalRepositoryTest.java
+++ src/test/java/com/example/loytokoti/AnimalRepositoryTest.java
@@ -1,15 +1,12 @@
 package com.example.loytokoti;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import com.example.loytokoti.domain.Animal;
 import com.example.loytokoti.domain.AnimalRepository;
 import com.example.loytokoti.domain.Species;
